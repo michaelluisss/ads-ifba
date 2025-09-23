b6db6ec3208fedfaaab87dc8000ad47f5e94b1a6
Michael <ldmichael838@gmail.com>
2025-09-23 12:45:31 -0300

    .

---
+++ codigos_de_exercicio/conversor_de_temperatura/conversor.c
@@ -0,0 +1,48 @@
+/*Exiba um menu com as seguintes opções:
+$ Celsius para Fahrenheit
+$ Fahrenheit para Celsius
+$ Sair
+Use um switch para processar a escolha do usuário.
+Para as opções de conversão, solicite a temperatura correspondente e realize a operação usando as fórmulas:
+Celsius para Fahrenheit: F=C×95+32
+Fahrenheit para Celsius: C=(F−32)×59
+Caso o usuário escolha uma opção inválida, exiba “Opção inválida”.*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    float celsius,far;
+    int valor;
+    do{
+    printf("1.Celsius para Fahrenheit\n");
+    printf("2.Fahrenheit para Celsius\n");
+    printf("3.Sair\n");
+    printf("Digite: ");
+    scanf("%d",&valor);
+    switch ( valor ){
+        case 1:
+        printf("Digite o valor em Celsius: ");
+        scanf("%f",&celsius);
+        far = (celsius * 9/5) + 32;
+
+        printf("%.2fF\n",far);
+
+        break;
+        case 2:
+        printf("Digite o valor em Fahrenheit: ");
+        scanf("%f",&far);
+        celsius = (far-32)*1.8;
+
+        printf("%fC\n",celsius);
+        break;
+    
+        default:
+        printf("numero invalido\n");
+        break;
+    }
+
+
+
+    }while(valor != 3);
+    
+    
+}
