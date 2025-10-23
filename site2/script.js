7a4a36ce097a4a57608671521be0c222005b2ed5
Michael <ldmichael838@gmail.com>
2025-10-22 23:15:47 -0300

    .

--- site2/script.js
+++ site2/script.js
@@ -1,13 +1,24 @@
 function tarefas(){
-    let inserir = document.getElementById("inserir");
-    let tarefa = inserir.value;
+    // Entradas do Usuario
+    let inserirLista = document.getElementById("inserirLista");
+    let tarefa = inserirLista.value;
 
-    
+    // Criando a lista
     let tarefaAdicionada = document.getElementById("ListaTarefa");
     let ListaTarefa = document.createElement("li");
     
-    ListaTarefa.textContent = tarefa;
-    tarefaAdicionada.appendChild(ListaTarefa);
-    inserir.value = "";
+    let texto = "Nada digitado!"
+    if(inserirLista.value.trim() === ""){
+        
+        document.getElementById("mensagem").textContent = texto;
+        
+        
+    }else{
+        ListaTarefa.textContent = tarefa;
+        tarefaAdicionada.appendChild(ListaTarefa);
+        texto = "Tarefa adicionada com sucesso!"
+        document.getElementById("mensagem").textContent = texto;
+    }
+    inserirLista.value = "";
 
 }
