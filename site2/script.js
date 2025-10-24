29144872a8d957a74aa4634aad40f02ee4fb3ce7
Michael <ldmichael838@gmail.com>
2025-10-24 17:47:37 -0300

    go

--- site2/script.js
+++ site2/script.js
@@ -1,24 +1,24 @@
-function tarefas(){
-    // Entradas do Usuario
-    let inserirLista = document.getElementById("inserirLista");
-    let tarefa = inserirLista.value;
+function tarefas() {
+  // Entradas do Usuario
+  let inserirLista = document.getElementById("inserirLista");
+  let tarefa = inserirLista.value;
 
-    // Criando a lista
-    let tarefaAdicionada = document.getElementById("ListaTarefa");
-    let ListaTarefa = document.createElement("li");
-    
-    let texto = "Nada digitado!"
-    if(inserirLista.value.trim() === ""){
-        
-        document.getElementById("mensagem").textContent = texto;
-        
-        
-    }else{
-        ListaTarefa.textContent = tarefa;
-        tarefaAdicionada.appendChild(ListaTarefa);
-        texto = "Tarefa adicionada com sucesso!"
-        document.getElementById("mensagem").textContent = texto;
-    }
-    inserirLista.value = "";
+  // Criando a lista
+  let tarefaAdicionada = document.getElementById("ListaTarefa");
+  let ListaTarefa = document.createElement("li");
 
+  let mensagem = document.getElementById("mensagem");
+  let texto ="";
+  if (inserirLista.value.trim() === "") {
+    texto = "Nada digitado!";
+    mensagem.textContent = texto;
+    mensagem.textContent.style.color("#a34743") = mensagem.textContent;
+  } else{
+    ListaTarefa.textContent = tarefa;
+    ListaTarefa.className = "Lista";
+    tarefaAdicionada.appendChild(ListaTarefa);
+    texto = "Tarefa adicionada com sucesso!";
+    mensagem.textContent = texto;
+  }
+  inserirLista.value = "";
 }
