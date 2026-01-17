86d16bf8c7f55efebe4f3e0453078c5e9aad831d
Michael <ldmichael838@gmail.com>
2026-01-17 07:06:14 -0300

    .

--- codigos_de_exercicio/Avaliacoes 3/q1.3.2025.c
+++ codigos_de_exercicio/Avaliacoes 3/q1.3.2025.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 #define TAM 50
-int main(){
+int main()
+{
     char text[TAM];
+    char cobol[5] = {'c', 'o', 'b', 'o', 'l'};
+    int j = 0;
     int i = 0;
+    int cont = 0;
     printf("Digite as 5 palavras separadas por hifen:\n");
-    fgets(text,TAM,stdin);
-    while (text[i] != '\0'){
-       
+    fgets(text, TAM, stdin);
+    while (i < 5)
+    {
+        while (text[j] != '-'){
+            
+            if ((j == 0  || text[j -1] == '-') || (text[j-1] == '-' || text[j+1] == '\n' || text[j+1] == '\0'))
+            {
+                if (text[j] == cobol[i]){
+                    cont++;
+                } 
+            }
+        
+            j++;
+        
+            
+        } i++;
+         j++;
     }
-    
+    if(cont == 5)
+    {
+        printf("COBOl");
+    }
+    else{
+        printf("BUG");
+    }
 }
